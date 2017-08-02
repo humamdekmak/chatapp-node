b2ab82b24b66aebdaf14066b8d4b5b4b57e938ce
Humam Dekmak <humamdekmak@gmail.com>
2017-08-02 21:23:03 +0000

    add connction

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-var app = express();
+const socketIO  = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    
+socket.on('disconnect', () =>{
+    console.log('disconnected');
+})
+});
+
+
+
 app.use(express.static(publicPath));
 
 
 
-app.listen(port,process.env.IP, () =>{
+server.listen(port,process.env.IP, () =>{
 console.log(`the server is listening ${port}`);
 })
